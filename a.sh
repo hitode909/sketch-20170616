33fe6a251a48b4e1575c18678edbfff9b1984a75
hitode909 <hitode909@gmail.com>
2017-06-18 16:07:30 +0900

    kill chrome, delete data

--- a.sh
+++ a.sh
@@ -7,13 +7,14 @@ mkdir -p $data_dir
 for url in "$@"
 do
   echo "$url"
-
   /usr/bin/google-chrome-unstable --disable-gpu --headless --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --user-data-dir=/data &
+  CHROME_PID=$!
   sleep 3
 
   output=$(node url-to-filename.js "$url")
   node index.js --full --delay 1000 --url "$url" --output "$data_dir/$output"
   node upload-to-gyazo.js "$data_dir/$output" "$url" "$gyazo_access_token" "$gyazo_collection_id"
-
-  killall google-chrome-unstable
+  kill $CHROME_PID
 done
+
+rm -rf $data_dir
